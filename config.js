fa4cdf1a53ac497338cb0d86d0692af627f6bcf7
jin1963 <kaojino89@gmail.com>
2025-07-19 19:06:26 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// Smart Contract ที่ใช้
+const CONTRACT_ADDRESS = "0x9B0Abb27524B2857322C2D3682c5bc819eDB6d73";
+
+// Reward Token ที่จะจ่าย (KJC) ← เอาไว้ใช้ใน main.js ถ้าต้องตรวจสอบเพิ่มเติม
+const REWARD_TOKEN_SYMBOL = "KJC";
+
+// ชื่อเว็บของเรา
+const BASE_URL = "https://jin1963.github.io/KDCreferal/"; // เปลี่ยนตาม GitHub Pages ของคุณ
